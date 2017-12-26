dea34910fc1b86f790a1969c2fbfea0fdf31cf1b
josescuderoh <josescuderoh@gmail.com>
2017-12-26 12:22:20 -0500

    Added gunicorn to deploy folder

--- deploy/server_setup.sh
+++ deploy/server_setup.sh
@@ -9,32 +9,34 @@ VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
 locale-gen en_GB.UTF-8
 
 # Install Python, SQLite and pip
-apt-get update
+sudo apt-get update
 sudo apt-get upgrade
-apt-get install -y python3-dev python-pip supervisor nginx git postgresql postgresql-contrib
+sudo apt-get install -y python3-dev python-pip supervisor nginx git
 
 # Upgrade pip to the latest version.
-pip install --upgrade pip
-pip install virtualenv
+sudo pip install --upgrade pip
+sudo pip install virtualenv
+sudo pip install gunicorn
 
-mkdir -p $PROJECT_BASE_PATH
-git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/prices_api
+sudo mkdir -p $PROJECT_BASE_PATH
+sudo git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/prices_api
 
-mkdir -p $VIRTUALENV_BASE_PATH
-virtualenv -p python3 $VIRTUALENV_BASE_PATH/prices_api
+sudo mkdir -p $VIRTUALENV_BASE_PATH
+sudo virtualenv -p python3 $VIRTUALENV_BASE_PATH/prices_api
+sudo chown -R ubuntu:ubuntu /usr/local/virtualenvs/prices_api
 
 source $VIRTUALENV_BASE_PATH/prices_api/bin/activate
 pip install -r $PROJECT_BASE_PATH/prices_api/requirements.txt
 
 # Setup Supervisor to run our uwsgi process.
-cp $PROJECT_BASE_PATH/prices_api/deploy/supervisor_prices_api.conf /etc/supervisor/conf.d/prices_api.conf
-supervisorctl reread
-supervisorctl update
-supervisorctl restart prices_api
+sudo cp $PROJECT_BASE_PATH/prices_api/deploy/supervisor_prices_api.conf /etc/supervisor/conf.d/prices_api.conf
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl restart prices_api
 
 # Setup nginx to make our application accessible.
-cp $PROJECT_BASE_PATH/prices_api/deploy/nginx_prices_api.conf /etc/nginx/sites-available/prices_api.conf
-ln -s /etc/nginx/sites-available/prices_api.conf /etc/nginx/sites-enabled/prices_api.conf
-systemctl restart nginx.service
+sudo cp $PROJECT_BASE_PATH/prices_api/deploy/nginx_prices_api.conf /etc/nginx/sites-available/prices_api.conf
+sudo ln -s /etc/nginx/sites-available/prices_api.conf /etc/nginx/sites-enabled/prices_api.conf
+sudo systemctl restart nginx.service
 
 echo "DONE! :)"
